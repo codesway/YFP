28fa16fb9434b8b40dcc522a62af6d01917d95d1
刘林亮 <liulinliang@mxtrip.cn>
2016-05-18 19:31:15 +0800

    login

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -3,4 +3,5 @@ return array(
 	//'配置项'=>'配置值'
 	'PUBLIC_PATH' => '../../../Public/',
 	'TMPL_ENGINE_TYPE' =>'PHP',
+	'URL_HTML_SUFFIX' => '',
 );
--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -16,4 +16,20 @@ class IndexController extends ControllerBase {
   public function login() {
     $this->display();
   }
+
+  public function dologin() {
+    echo 'aa'; exit();
+    if (!IS_POST) {
+      $this->error('非法请求');
+    }
+
+    $username = I('username', '');
+    $password = I('password', '');
+    $code = I('code');
+    if (empty($username) || empty($password) || empty($code)) {
+      $this->error('参数错误, 请重新输入');
+    }
+
+
+  }
 }
