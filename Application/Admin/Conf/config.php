0a5b2890f48f3f76c67975159237d3ecb719633f
刘林亮 <liulinliang@mxtrip.cn>
2016-05-18 18:51:28 +0800

    update

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -1,6 +1,6 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-	'PUBLIC_PATH' => PUBLIC_PATH . '/',
+	'PUBLIC_PATH' => '../../../Public/',
 	'TMPL_ENGINE_TYPE' =>'PHP',
 );
--- index.php
+++ index.php
@@ -15,7 +15,6 @@
 if(version_compare(PHP_VERSION,'5.6.0','<'))  die('require PHP > 5.6.0 !');
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
-
 // 定义应用目录
 define('APP_PATH','./Application/');
 
