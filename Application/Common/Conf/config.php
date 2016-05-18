63f0460ef4d5ed03d0a0221d9b3bb2d9614dcd7d
刘林亮 <liulinliang@mxtrip.cn>
2016-05-18 14:41:20 +0800

    init

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,13 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	'SHOW_PAGE_TRACE' => true,
+	// 'APP_SUB_DOMAIN_DEPLOY' => 1,
+	// 'APP_SUB_DOMAIN_RULES' => array(
+	// 	'admin' => array('Admin/')
+	// ),
+	'MODULE_DENY_LIST' => array(
+		'Common', 'Runtime', 'User'
+	),
+	
 );
